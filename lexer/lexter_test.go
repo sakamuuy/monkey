3dff33dc608073ba03cde8fcab99bbc986c92747
sakamuuy <sakamuu.y@gmail.com>
2022-03-03 09:31:48 +0900

    Expansion test case

--- lexer/lexter_test.go
+++ lexer/lexter_test.go
@@ -1,25 +1,64 @@
 package lexer
 
 import (
+	"fmt"
 	"monkey/token"
 	"testing"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x,y) {
+	x + y;
+};
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.INDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.INDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.INDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+
 		{token.LPAREN, "("},
+		{token.INDENT, "x"},
+		{token.COMMA, ","},
+		{token.INDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.INDENT, "x"},
+		{token.PLUS, "+"},
+		{token.INDENT, "y"},
 		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		{token.LET, "let"},
+		{token.INDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.INDENT, "add"},
+		{token.LPAREN, "("},
+		{token.INDENT, "five"},
+		{token.COMMA, ","},
+		{token.INDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
 	}
 
@@ -27,6 +66,7 @@ func TestNextToken(t *testing.T) {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
+		fmt.Printf("%v", tok.Literal)
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
